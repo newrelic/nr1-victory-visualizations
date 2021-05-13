612e28eba3754a83a14a170ee878e744748fcd90
Tim Glaser <tglaser@newrelic.com>
2021-05-13 10:07:18 -1000

    feat(ProgressBar): add Victory charts example verbatim

--- visualizations/progress-bar/index.js
+++ visualizations/progress-bar/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Radar,
-  RadarChart,
-  PolarGrid,
-  PolarAngleAxis,
-  PolarRadiusAxis,
-} from 'recharts';
+import { VictoryPie, VictoryAnimation, VictoryLabel } from 'victory';
 import {
   Card,
   CardBody,
@@ -21,17 +15,6 @@ export default class ProgressBarVisualization extends React.Component {
   // the nr1.json file for the visualization. See docs for more details.
   static propTypes = {
     /**
-     * A fill color to override the default fill color. This is an example of
-     * a custom chart configuration.
-     */
-    fill: PropTypes.string,
-
-    /**
-     * A stroke color to override the default stroke color. This is an example of
-     * a custom chart configuration.
-     */
-    stroke: PropTypes.string,
-    /**
      * An array of objects consisting of a nrql `query` and `accountId`.
      * This should be a standard prop for any NRQL based visualizations.
      */
@@ -42,6 +25,37 @@ export default class ProgressBarVisualization extends React.Component {
       })
     ),
   };
+
+  constructor() {
+    super();
+    this.state = {
+      percent: 25,
+      data: this.getData(0),
+    };
+  }
+
+  componentDidMount() {
+    let percent = 25;
+    this.setStateInterval = window.setInterval(() => {
+      percent += Math.random() * 25;
+      percent = percent > 100 ? 0 : percent;
+      this.setState({
+        percent,
+        data: this.getData(percent),
+      });
+    }, 2000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.setStateInterval);
+  }
+
+  getData(percent) {
+    return [
+      { x: 1, y: percent },
+      { x: 2, y: 100 - percent },
+    ];
+  }
 
   /**
    * Restructure the data for a non-time-series, facet-based NRQL query into a
@@ -64,7 +78,7 @@ export default class ProgressBarVisualization extends React.Component {
   };
 
   render() {
-    const { nrqlQueries, stroke, fill } = this.props;
+    const { nrqlQueries } = this.props;
 
     const nrqlQueryPropsAvailable =
       nrqlQueries &&
@@ -96,21 +110,42 @@ export default class ProgressBarVisualization extends React.Component {
               const transformedData = this.transformData(data);
 
               return (
-                <RadarChart
-                  width={width}
-                  height={height}
-                  data={transformedData}
-                >
-                  <PolarGrid />
-                  <PolarAngleAxis dataKey="name" />
-                  <PolarRadiusAxis tickFormatter={this.formatTick} />
-                  <Radar
-                    dataKey="value"
-                    stroke={stroke || '#51C9B7'}
-                    fill={fill || '#51C9B7'}
-                    fillOpacity={0.6}
-                  />
-                </RadarChart>
+                <div>
+                  <svg viewBox="0 0 400 400" width={width} height={height}>
+                    <VictoryPie
+                      standalone={false}
+                      animate={{ duration: 1000 }}
+                      width={400}
+                      height={400}
+                      data={this.state.data}
+                      innerRadius={120}
+                      cornerRadius={25}
+                      labels={() => null}
+                      style={{
+                        data: {
+                          fill: ({ datum }) => {
+                            const color = datum.y > 30 ? 'green' : 'red';
+                            return datum.x === 1 ? color : 'transparent';
+                          },
+                        },
+                      }}
+                    />
+                    <VictoryAnimation duration={1000} data={this.state}>
+                      {(newProps) => {
+                        return (
+                          <VictoryLabel
+                            textAnchor="middle"
+                            verticalAnchor="middle"
+                            x={200}
+                            y={200}
+                            text={`${Math.round(newProps.percent)}%`}
+                            style={{ fontSize: 45 }}
+                          />
+                        );
+                      }}
+                    </VictoryAnimation>
+                  </svg>
+                </div>
               );
             }}
           </NrqlQuery>
