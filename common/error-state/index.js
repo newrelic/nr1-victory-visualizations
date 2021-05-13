231d70900b5e19bf65db0118f90d3710277bd1a8
Tim Glaser <tglaser@newrelic.com>
2021-05-13 13:07:13 -1000

    feat(ErrorState): create generic error state component

---
+++ common/error-state/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card, CardBody, HeadingText } from 'nr1';
+
+const ErrorState = () => (
+  <Card className="ErrorState">
+    <CardBody className="ErrorState-cardBody">
+      <HeadingText
+        className="ErrorState-headingText"
+        spacingType={[HeadingText.SPACING_TYPE.LARGE]}
+        type={HeadingText.TYPE.HEADING_3}
+      >
+        Oops! Something went wrong.
+      </HeadingText>
+    </CardBody>
+  </Card>
+);
+
+export default ErrorState;
