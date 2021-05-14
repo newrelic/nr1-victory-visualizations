976ca6b7d5919977abb412bcde7787676e18bf00
Cayla Hamann <chamann@newrelic.com>
2021-05-14 17:11:31 -0400

    chore: simplify data transform

--- visualizations/multifacet-bar-chart/index.js
+++ visualizations/multifacet-bar-chart/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { VictoryChart, VictoryGroup, VictoryBar, VictoryAxis } from "victory";
 
 import {
   Card,
@@ -8,19 +9,72 @@ import {
   NrqlQuery,
   Spinner,
   AutoSizer,
-} from 'nr1';
-import { VictoryBar, VictoryChart, VictoryGroup } from 'victory';
+} from "nr1";
 
-export default class MultifacetBarChartVisualization extends React.Component {
+const getNumBuckets = (data) => {
+  return data.reduce((acc, curr) => acc + curr.length, 0);
+}
+
+export default class VictoryBarChartVisualization extends React.Component {
   // Custom props you wish to be configurable in the UI must also be defined in
   // the nr1.json file for the visualization. See docs for more details.
   static propTypes = {
+    /**
+     * An array of objects consisting of a nrql `query` and `accountId`.
+     * This should be a standard prop for any NRQL based visualizations.
+     */
     nrqlQueries: PropTypes.arrayOf(
       PropTypes.shape({
         accountId: PropTypes.number,
         query: PropTypes.string,
       })
     ),
+  };
+
+  /**
+   * Group the data by facet.
+   * Shape of result:
+   *  [
+   *    [
+   *        { x: "us-west, production", y: 1, groupLabel: 'catalog-service' },
+   *        { x: "us-west, eu-production", y: 2, groupLabel: 'catalog-service' },
+   *        { x: "us-east, production", y: 1, groupLabel: 'catalog-service' },
+   *        { x: "us-east, eu-production", y: 2, groupLabel: 'catalog-service' },
+   *    ],
+   *    ...
+   *  ]
+   *
+   * Handles "Other" facet
+   *
+   * TODO: rewrite with reduce
+   * TODO: try relying on the data accessor approach (does it support array index access?)
+   *
+   */
+  transformData = (data) => {
+
+    //create an object that maps all of the partitions that colorFacets into groupings by the labelFacets
+    const facetGroups = data.reduce((acc, curr)=> {
+      const {metadata, data} = curr; 
+      const [xLabelFacet, colorFacet] = metadata.name.split(',').map(name => name.trim()); 
+
+      if (acc[colorFacet]) {
+        acc[colorFacet][xLabelFacet] = data[0].y
+      } else {
+        acc[colorFacet] = {[xLabelFacet]: data[0].y}
+      }
+
+      return acc;
+      
+    },{})
+
+    //transform this into an array of arrays that can be read by Victory
+    const transformed = Object.entries(facetGroups).map(([colorFacet, entry]) => {
+      return Object.entries(entry).map(([key, value]) => ({groupLabel: colorFacet, x: key, y: value})); 
+    })
+
+    console.log({transformed})
+
+    return transformed;
   };
 
   render() {
@@ -36,8 +90,6 @@ export default class MultifacetBarChartVisualization extends React.Component {
       return <EmptyState />;
     }
 
-    const barData = () => [1, 2, 3,].map((el) => ({x: el, y: Math.random()*10}));
-
     return (
       <AutoSizer>
         {({ width, height }) => (
@@ -52,21 +104,28 @@ export default class MultifacetBarChartVisualization extends React.Component {
               }
 
               if (error) {
+                console.log({ error });
                 return <ErrorState />;
               }
 
+              const transformedData = this.transformData(data);
+
+              const numBuckets = getNumBuckets(transformedData); 
+
+              //width of labels 
+              const offsetX = 80; 
+              //space between each bar
+              const spaceBetweenBars = 5;
+              const barWidth = (width/(numBuckets+50))
+
               return (
-                <div>
-                  <VictoryChart
-                    domainPadding={{ x: 50 }}
-                    width={width}
-                    height={height}
-                  >
-                    <VictoryGroup offset={20} style={{ data: { width: 15 } }} colorScale={"qualitative"}>
-                      {[1,2,3,].map(() => <VictoryBar data={barData()}/>)}
-                    </VictoryGroup>
-                  </VictoryChart>
-                </div>
+                <VictoryChart domainPadding={{ x: offsetX }} width={width} height={height} padding={{ top: 20, bottom: 40, left: 80, right: 20 }}>
+                  <VictoryGroup offset={barWidth+spaceBetweenBars} width={width} style={{data: {width: barWidth}}} colorScale={"qualitative"}>
+                    {transformedData.map((series) => (
+                      <VictoryBar data={series} />
+                    ))}
+                  </VictoryGroup>
+                </VictoryChart>
               );
             }}
           </NrqlQuery>
@@ -83,7 +142,7 @@ const EmptyState = () => (
         spacingType={[HeadingText.SPACING_TYPE.LARGE]}
         type={HeadingText.TYPE.HEADING_3}
       >
-        Please provide at least one NRQL query & account ID pair
+        Please provide a NRQL query & account ID pair
       </HeadingText>
       <HeadingText
         spacingType={[HeadingText.SPACING_TYPE.MEDIUM]}
@@ -91,7 +150,9 @@ const EmptyState = () => (
       >
         An example NRQL query you can try is:
       </HeadingText>
-      <code>FROM NrUsage SELECT sum(usage) FACET metric SINCE 1 week ago</code>
+      <code>
+        FROM Transaction SELECT average(duration) FACET environment, appName
+      </code>
     </CardBody>
   </Card>
 );
