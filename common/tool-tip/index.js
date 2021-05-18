f8e0be6842eae153b10eab69eef2272427885777
Cayla Hamann <chamann@newrelic.com>
2021-05-18 16:49:56 -0400

    chore: center the tooltip

--- common/tool-tip/index.js
+++ common/tool-tip/index.js
@@ -7,11 +7,14 @@ TODO:
 2. center pointer to stack
 */
 
+const TOOLTIP_PADDING = 8;
+
 class Tooltip extends React.Component {
   static defaultEvents = VictoryTooltip.defaultEvents;
   render() {
     const {datum, scale} = this.props; 
-    const diff = (datum._y0 - datum.y)/2
+    const diff = Math.abs(datum._y1 - datum._y0) / 2 + datum._y0
+    const scaledY = scale.y(diff) + TOOLTIP_PADDING
     return (
       <VictoryTooltip
         {...this.props}
@@ -22,8 +25,8 @@ class Tooltip extends React.Component {
           fill: `#f7f8f8`,
           filter: `drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.32))`,
         }}
-        y={scale.y(diff)}
-        flyoutPadding={8}
+        y={scaledY}
+        flyoutPadding={TOOLTIP_PADDING}
         style={[
           {
             color: '#3e4c4c',
