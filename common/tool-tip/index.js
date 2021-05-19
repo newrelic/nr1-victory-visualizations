940bff362bc316539e3afacb1dcb1c24fddce351
Cayla Hamann <chamann@newrelic.com>
2021-05-18 20:08:35 -0400

    chore: remove lineheight since that is not an accepted prop

--- common/tool-tip/index.js
+++ common/tool-tip/index.js
@@ -7,25 +7,23 @@ const TOOLTIP_PADDING = 16;
 ** TO DO **
   1. replace with CSS vars for `flyoutStyle`: 
   {
-    stroke: datum?.color ? 'var(--nr1--base-colors--ui--gray--2)', 
+    stroke: datum?.color ? 'var(--nr1--colors--background--surface)', 
     strokeWidth: 2, 
-    fill: 'var(--nr1--base-colors--ui--gray--2)',
+    fill: 'var(--nr1--colors--background--surface)',
     filter: 'drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.32))'
   }
 
   and for `style`:
   [
     {
-      color: 'var(--nr1--typography--heading--6--color)',
-      fontSize: 'var(--nr1--typography--heading--6--font-size)',
-      fontWeight: 'var(--nr1--typography--heading--6--font-weight)',
-      fontFamily: 'var(--nr1--typography--heading--6--font-family)',
-      lineHeight:  'var(--nr1--typography--heading--6--line-height)',
+      color: 'var(--nr1--typography--heading--5--color)',
+      fontSize: 'var(--nr1--typography--heading--5--font-size)',
+      fontFamily: 'var(--nr1--typography--heading--5--font-family)',
     },
     {
       color: 'var(--nr1--colors--text--secondary)',
       fontSize: 'var(--nr1--typography--body--1--font-size)',
-      fontFamily: 'var(--nr1--typography--heading--6--font-family)',
+      fontFamily: 'var(--nr1--typography--body--1--font-family)',
       baselineShift: 'sub',
     },
   ]
@@ -51,8 +49,8 @@ class Tooltip extends React.Component {
         horizontal
         flyoutStyle={{
           strokeWidth: 2,
-          stroke: datum?.color ?? `#f7f8f8`,
-          fill: `#f7f8f8`,
+          stroke: datum?.color ?? `#fff`,
+          fill: `#fff`,
           filter: `drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.32))`,
         }}
         y={scaledY}
@@ -63,7 +61,6 @@ class Tooltip extends React.Component {
             fontSize: '12px',
             fontWeight: 600,
             fontFamily: `Open Sans,"Segoe UI","Tahoma",sans-serif`,
-            lineHeight: '16px'
           },
           {
             color: '#223030',
