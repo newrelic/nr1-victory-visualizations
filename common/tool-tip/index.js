2bcfb1647ae8c69ef842d0a65f16ebd43e00ba99
Cayla Hamann <chamann@newrelic.com>
2021-05-18 20:00:21 -0400

    chore: updates to organization

--- common/tool-tip/index.js
+++ common/tool-tip/index.js
@@ -3,6 +3,34 @@ import { VictoryTooltip } from 'victory';
 
 const TOOLTIP_PADDING = 16;
 
+/*
+** TO DO **
+  1. replace with CSS vars for `flyoutStyle`: 
+  {
+    stroke: datum?.color ? 'var(--nr1--base-colors--ui--gray--2)', 
+    strokeWidth: 2, 
+    fill: 'var(--nr1--base-colors--ui--gray--2)',
+    filter: 'drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.32))'
+  }
+
+  and for `style`:
+  [
+    {
+      color: 'var(--nr1--typography--heading--6--color)',
+      fontSize: 'var(--nr1--typography--heading--6--font-size)',
+      fontWeight: 'var(--nr1--typography--heading--6--font-weight)',
+      fontFamily: 'var(--nr1--typography--heading--6--font-family)',
+      lineHeight:  'var(--nr1--typography--heading--6--line-height)',
+    },
+    {
+      color: 'var(--nr1--colors--text--secondary)',
+      fontSize: 'var(--nr1--typography--body--1--font-size)',
+      fontFamily: 'var(--nr1--typography--heading--6--font-family)',
+      baselineShift: 'sub',
+    },
+  ]
+*/
+
 class Tooltip extends React.Component {
   static defaultEvents = VictoryTooltip.defaultEvents;
   render() {
@@ -31,10 +59,11 @@ class Tooltip extends React.Component {
         flyoutPadding={TOOLTIP_PADDING}
         style={[
           {
-            color: '#3e4c4c',
-            fontSize: '14px',
+            color: '#223030',
+            fontSize: '12px',
             fontWeight: 600,
             fontFamily: `Open Sans,"Segoe UI","Tahoma",sans-serif`,
+            lineHeight: '16px'
           },
           {
             color: '#223030',
