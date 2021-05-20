3c467f1e3415914ffa92ba1b3094a7a74e589248
Jerel Miller <jmiller@newrelic.com>
2021-05-20 16:41:30 -0700

    feat(truncate-label): Create a truncateLabel helper to truncate text to a particular width

---
+++ src/utils/truncate-label.js
@@ -0,0 +1,46 @@
+const NARROW_CHARS = ['i', 'l', 'j', ',', '.', ';', '!', '1'];
+const DEFAULT_APPROX_CHAR_WIDTH = 10;
+const APPROX_NARROW_CHAR_RATIO = 0.5;
+
+const guessCharWidth = (char, { approxCharWidth }) =>
+  NARROW_CHARS.includes(char)
+    ? approxCharWidth * APPROX_NARROW_CHAR_RATIO
+    : approxCharWidth;
+
+const getTruncatedText = (chars, allocatedWidth, { approxCharWidth }) => {
+  const { text } = chars.reduce(
+    ({ totalWidth, text }, char) => {
+      const charWidth = guessCharWidth(char, { approxCharWidth });
+      const newWidth = totalWidth + charWidth;
+
+      return {
+        totalWidth: newWidth,
+        text: newWidth > allocatedWidth ? text : text + char,
+      };
+    },
+    { totalWidth: 0, text: '' }
+  );
+
+  return text;
+};
+
+const truncateLabel = (
+  text,
+  allocatedWidth,
+  { approxCharWidth = DEFAULT_APPROX_CHAR_WIDTH } = {}
+) => {
+  const chars = text.split('');
+  const guessedTextWidth = chars.reduce(
+    (totalWidth, char) =>
+      totalWidth + guessCharWidth(char, { approxCharWidth }),
+    0
+  );
+
+  console.log({ text, guessedTextWidth, allocatedWidth, approxCharWidth });
+
+  return guessedTextWidth > allocatedWidth
+    ? `${getTruncatedText(chars, allocatedWidth, { approxCharWidth })}...`
+    : text;
+};
+
+export default truncateLabel;
