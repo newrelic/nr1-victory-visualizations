8148a79c7aa57626094d65b89ae43c06b2fbd17e
Jerel Miller <jmiller@newrelic.com>
2021-05-21 10:27:57 -0700

    chore: Update visualization names to agreed names

--- visualizations/stacked-bar-chart/index.js
+++ visualizations/stacked-bar-chart/index.js
@@ -56,7 +56,7 @@ const getBarCount = (data) => {
   }, new Set()).size;
 };
 
-export default class MultiFacetBarChartVisualization extends React.Component {
+export default class StackedBarChart extends React.Component {
   // Custom props you wish to be configurable in the UI must also be defined in
   // the nr1.json file for the visualization. See docs for more details.
   static propTypes = {
@@ -162,7 +162,7 @@ export default class MultiFacetBarChartVisualization extends React.Component {
     }
 
     return (
-      <AutoSizer className="MultiFacetBarChartVisualization">
+      <AutoSizer className="StackedBarChart">
         {({ width, height }) => (
           <NrqlQuery
             query={nrqlQueries[0].query}
