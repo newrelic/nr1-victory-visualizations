778ae5de2be4ef25c2dcde2b11467a7d4f9096b9
kyang <kyang@newrelic.com>
2021-06-16 11:28:18 -0700

    refactor: Add NrqlQueryError handling
    Co-authored-by: Katherine Kelly <kat-star@users.noreply.github.com>

--- visualizations/scatter-plot-chart/index.js
+++ visualizations/scatter-plot-chart/index.js
@@ -1,121 +1,124 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-    Card,
-    CardBody,
-    HeadingText,
-    NrqlQuery,
-    Spinner,
-    AutoSizer,
-  } from 'nr1';
+	Card,
+	CardBody,
+	HeadingText,
+	NrqlQuery,
+	Spinner,
+	AutoSizer,
+} from 'nr1';
 import { VictoryChart, VictoryScatter, VictoryTheme } from 'victory';
+import NrqlQueryError from '../../src/nrql-query-error/nrql-query-error';
 
 export default class ScatterPlotChartVisualization extends React.Component {
-    // Custom props you wish to be configurable in the UI must also be defined in
-    // the nr1.json file for the visualization. See docs for more details.
-    static propTypes = {
-        /**
-         * An array of objects consisting of a nrql `query` and `accountId`.
-         * This should be a standard prop for any NRQL based visualizations.
-         */
-        nrqlQueries: PropTypes.arrayOf(
-            PropTypes.shape({
-                accountId: PropTypes.number,
-                query: PropTypes.string,
-            })
-        ),
-    };
+	// Custom props you wish to be configurable in the UI must also be defined in
+	// the nr1.json file for the visualization. See docs for more details.
+	static propTypes = {
+		/**
+		 * An array of objects consisting of a nrql `query` and `accountId`.
+		 * This should be a standard prop for any NRQL based visualizations.
+		 */
+		nrqlQueries: PropTypes.arrayOf(
+			PropTypes.shape({
+				accountId: PropTypes.number,
+				query: PropTypes.string,
+			})
+		),
+	};
 
-    render() {
-        const { nrqlQueries } = this.props;
+	render() {
+		const { nrqlQueries } = this.props;
 
-        const nrqlQueryPropsAvailable =
-            nrqlQueries &&
-            nrqlQueries[0] &&
-            nrqlQueries[0].accountId &&
-            nrqlQueries[0].query;
+		const nrqlQueryPropsAvailable =
+			nrqlQueries &&
+			nrqlQueries[0] &&
+			nrqlQueries[0].accountId &&
+			nrqlQueries[0].query;
 
-        if (!nrqlQueryPropsAvailable) {
-            return <EmptyState />;
-        }
+		if (!nrqlQueryPropsAvailable) {
+			return <EmptyState />;
+		}
 
-        return (
-            <AutoSizer>
-                {({ width, height }) => (
-                    <NrqlQuery
-                        query={nrqlQueries[0].query}
-                        accountId={parseInt(nrqlQueries[0].accountId)}
-                        pollInterval={NrqlQuery.AUTO_POLL_INTERVAL}
-                    >
-                        {({ data, loading, error }) => {
-                            if (loading) {
-                                return <Spinner />;
-                            }
+		return (
+			<AutoSizer>
+				{({ width, height }) => (
+					<NrqlQuery
+						query={nrqlQueries[0].query}
+						accountId={parseInt(nrqlQueries[0].accountId)}
+						pollInterval={NrqlQuery.AUTO_POLL_INTERVAL}
+					>
+						{({ data, loading, error }) => {
+							if (loading) {
+								return <Spinner />;
+							}
 
-                            if (error) {
-                                return <ErrorState />;
-                            }
+							if (error && data === null) {
+								return <NrqlQueryError
+									title='NRQL Syntax Error'
+									description={error.message}
+								/>;
+							}
+							console.log('data', data);
 
-                            console.log('data', data);
-
-                            return (
-                                <VictoryChart
-                                    theme={VictoryTheme.material}
-                                    domain={{ x: [0, 5], y: [0, 7] }}
-                                >
-                                    <VictoryScatter
-                                        style={{ data: { fill: "#c43a31" } }}
-                                        size={7}
-                                        data={[
-                                            { x: 1, y: 2 },
-                                            { x: 2, y: 3 },
-                                            { x: 3, y: 5 },
-                                            { x: 4, y: 4 },
-                                            { x: 5, y: 7 }
-                                        ]}
-                                    />
-                                </VictoryChart>
-                            );
-                        }}
-                    </NrqlQuery>
-                )}
-            </AutoSizer>
-        );
-    }
+							return (
+								<VictoryChart
+									theme={VictoryTheme.material}
+									domain={{ x: [0, 5], y: [0, 7] }}
+								>
+									<VictoryScatter
+										style={{ data: { fill: "#c43a31" } }}
+										size={7}
+										data={[
+											{ x: 1, y: 2 },
+											{ x: 2, y: 3 },
+											{ x: 3, y: 5 },
+											{ x: 4, y: 4 },
+											{ x: 5, y: 7 }
+										]}
+									/>
+								</VictoryChart>
+							);
+						}}
+					</NrqlQuery>
+				)}
+			</AutoSizer>
+		);
+	}
 }
 
 const EmptyState = () => (
-    <Card className="EmptyState">
-        <CardBody className="EmptyState-cardBody">
-            <HeadingText
-                spacingType={[HeadingText.SPACING_TYPE.LARGE]}
-                type={HeadingText.TYPE.HEADING_3}
-            >
-                Please provide at least one NRQL query & account ID pair
-            </HeadingText>
-            <HeadingText
-                spacingType={[HeadingText.SPACING_TYPE.MEDIUM]}
-                type={HeadingText.TYPE.HEADING_4}
-            >
-                An example NRQL query you can try is:
-            </HeadingText>
-            <code>
-                FROM NrUsage SELECT sum(usage) FACET metric SINCE 1 week ago
-            </code>
-        </CardBody>
-    </Card>
+	<Card className="EmptyState">
+		<CardBody className="EmptyState-cardBody">
+			<HeadingText
+				spacingType={[HeadingText.SPACING_TYPE.LARGE]}
+				type={HeadingText.TYPE.HEADING_3}
+			>
+				Please provide at least one NRQL query & account ID pair
+			</HeadingText>
+			<HeadingText
+				spacingType={[HeadingText.SPACING_TYPE.MEDIUM]}
+				type={HeadingText.TYPE.HEADING_4}
+			>
+				An example NRQL query you can try is:
+			</HeadingText>
+			<code>
+				SELECT contentLength, duration, externalCallCount FROM EventType
+			</code>
+		</CardBody>
+	</Card>
 );
 
 const ErrorState = () => (
-    <Card className="ErrorState">
-        <CardBody className="ErrorState-cardBody">
-            <HeadingText
-                className="ErrorState-headingText"
-                spacingType={[HeadingText.SPACING_TYPE.LARGE]}
-                type={HeadingText.TYPE.HEADING_3}
-            >
-                Oops! Something went wrong.
-            </HeadingText>
-        </CardBody>
-    </Card>
+	<Card className="ErrorState">
+		<CardBody className="ErrorState-cardBody">
+			<HeadingText
+				className="ErrorState-headingText"
+				spacingType={[HeadingText.SPACING_TYPE.LARGE]}
+				type={HeadingText.TYPE.HEADING_3}
+			>
+				Oops! Something went wrong.
+			</HeadingText>
+		</CardBody>
+	</Card>
 );
